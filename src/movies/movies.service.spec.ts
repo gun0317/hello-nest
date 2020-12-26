93c3fe874952ada4a8deb193a3a265799daf0ca9
이영건 <ygun.lee@navercorp.com>
2020-12-26 17:44:45 +0900

    3.2 UnitTesting movies.service

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,9 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('MoviesService', () => {
   let service: MoviesService;
 
+  // tested before each 'it' test
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [MoviesService],
@@ -12,7 +15,36 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  // individual test
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll()', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne()', () => {
+    it('should return a movie', () => {
+      service.createOne({
+        title: 'testMovie1',
+        genres: ['test1'],
+        year: 2000,
+      });
+      const movie: Movie = service.getOne(1);
+      expect(movie).toBeDefined();
+    });
+
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(1);
+      } catch (err) {
+        expect(err).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
